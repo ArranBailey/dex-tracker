5814d30df1df293ba3ec218a7ce3c98b3643a4c5
Arran Bailey <arran2.bailey@live.uwe.ac.uk>
2025-05-12 21:58:01 +0100

    Added temp Search button

--- app/src/main/java/com/arranbailey/dextracker/MainActivity.kt
+++ app/src/main/java/com/arranbailey/dextracker/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -28,11 +29,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -142,6 +145,8 @@ class CardViewModel : ViewModel() {
     }
 }
 
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardSearchScreen(viewModel: CardViewModel = viewModel()) {
@@ -150,15 +155,18 @@ fun CardSearchScreen(viewModel: CardViewModel = viewModel()) {
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         TextField(
             value = searchQuery,
-            onValueChange = {
-                searchQuery = it
-                viewModel.search(it)
-            },
+            onValueChange = { searchQuery = it },
             label = { Text("Search Pokémon") },
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = { viewModel.search(searchQuery) },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Search")
+        }
 
         if (viewModel.isLoading.value) {
             CircularProgressIndicator()
